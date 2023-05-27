f5277008c112c702fb8aadb7918aef810dad8941
Renato Dias <116497642+rldias0288@users.noreply.github.com>
2023-05-27 18:33:26 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,25 @@
+terraform {
+  ## Definação da plataforma utilizada (Ex: Aws, Azure)
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
+  required_version = ">= 1.2.0"
+}
 
+  provider "aws" {
+    region  = "us-west-2"
+}
+
+  resource "aws_instance" "app_server" {
+    ## Acessar o painel da AWS e copiar o ami
+    ami           = "ami-03f65b8614a860c29" 
+    instance_type = "t2.micro"
+
+  tags = {
+    Name = "Primeira instancia"
+  }
+}
