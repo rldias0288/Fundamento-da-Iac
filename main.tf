054bc53ceef8d0cec31b42520751849f90c3ffe2
Renato Dias <116497642+rldias0288@users.noreply.github.com>
2023-05-31 16:02:51 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,12 @@ terraform {
     ## Acessar o painel da AWS e copiar o ami
     ami           = "ami-03f65b8614a860c29" 
     instance_type = "t2.micro"
-
+    user_data = <<-EOF
+                  #!/bin/bash
+                  cd /home/ubuntu
+                  echo "<h1>Feito com Terraform - 2 </h1>" > index.html
+                  nohup busybox httpd -f -p 8080 &
+                EOF    
   tags = {
     Name = "Primeira instancia"
   }
